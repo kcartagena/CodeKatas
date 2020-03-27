0f9bd242549a4e9b204a6a7d7ae7e26bc39311b7
Katie <52000656+kcartagena@users.noreply.github.com>
2020-03-27 00:37:55 -0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,10 @@
+function stringCalculator (){
+    let results = 0;
+    return {
+        value: function()
+            return 0;
+    },
+    add: function(num){
+        results = num + results;
+    }
+}
