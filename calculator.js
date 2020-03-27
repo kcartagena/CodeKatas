07846f3cedb76b3cea340cbeae77ca8ffcfdb02c
Katie <52000656+kcartagena@users.noreply.github.com>
2020-03-27 13:11:11 -0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,8 @@
+function stringCalculator (){
+	let results = 0;
+    return{
+        value: function(){
+            return results;
+        }
+    }
+}
---
+++ test.js
@@ -0,0 +1,32 @@
+describe('String Calculator', function() {
+    var calculator;
+
+beforeEach(function(){
+    calculator = stringCalculator();
+
+
+	it('empty string', function() {
+		calculator.add(0);
+		expect(calculator.value()).toEqual(0);
+		});
+
+	it('can take one value', function() {
+		calculator.add(1);
+		expect(calculator.value()).toEqual(2);
+	});
+
+	it('can take two values', function() {
+		calculator.add(1);
+		calculator.add(2);
+		expect(calculator.value()).toEqual(3);
+	});
+
+	it('can take unknown amount of values', function() {
+		calculator.add(1);
+		calculator.add(2);
+		calculator.add(3);
+		calculator.add(4);
+		expect(calculator.value()).toEqual(10);
+		});
+	});
+});
